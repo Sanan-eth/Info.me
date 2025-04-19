5729c34d6bce6067dbf6a1ec22d131eddfed8ae8
Sanan Ahmed <connect.sanan@gmail.com>
2025-04-19 15:42:36 +0500

    Add files via upload

---
+++ Info.me/info.dart
@@ -0,0 +1,14 @@
+void main() { 
+String name = "Sanan Ahmed"; 
+int age = 25; 
+String studentClass = "Graduated"; 
+double percentage = 99.999; 
+String city = "Karachi"; 
+
+
+print(name); 
+print(age); 
+print(studentClass); 
+print(percentage); 
+print(city); 
+}
